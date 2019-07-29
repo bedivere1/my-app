b51212f247c55d3ddadc7e6b9671cd9416d75a87
Phiratath Nopratanawong <phiratath.nop@gmail.com>
2019-07-29 23:13:09 +0700

    Updates

---
+++ precache-manifest.ec0040de88d19a1fad3c7caa2b8b4e15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eae0297808a99dc06d605016ee7018d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62374f56adbaa6ae79d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e65e01933d603cd04a13",
+    "url": "/static/js/2.e99b71ae.chunk.js"
+  },
+  {
+    "revision": "a62374f56adbaa6ae79d",
+    "url": "/static/js/main.ccf2d105.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
